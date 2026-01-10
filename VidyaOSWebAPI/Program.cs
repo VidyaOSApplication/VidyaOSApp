94e3e256c45372a05cd1aaaee1b787a507ccf992
dhruvurgr88 <dhruvpatelgurjar@gmail.com>
2026-01-10 14:58:02 +0530

    Client App

--- VidyaOSWebAPI/Program.cs
+++ VidyaOSWebAPI/Program.cs
@@ -19,6 +19,20 @@ namespace VidyaOSWebAPI
             // Add services to the container.
 
             builder.Services.AddControllers();
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowIonicApp", policy =>
+                {
+                    policy
+                        .WithOrigins(
+                            "http://localhost:8100",   // Ionic dev
+                            "http://localhost:4200"    // Angular (optional)
+                        )
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowCredentials();
+                });
+            });
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
@@ -65,6 +79,9 @@ namespace VidyaOSWebAPI
             }
 
             app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseCors("AllowIonicApp");
+
 
             app.UseAuthorization();
 
