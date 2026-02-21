84b4a1f775b253bd043ac39b3fd6b1f1276420ea
dhruvurgr88 <dhruvpatelgurjar@gmail.com>
2026-02-21 15:21:19 +0530

    Streams added

---
+++ VidyaOSDAL/DTOs/StreamDropdownDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VidyaOSDAL.DTOs
+{
+    public class StreamDropdownDto
+    {
+        public int StreamId { get; set; }     // PK from Streams table
+        public string StreamName { get; set; } = string.Empty; // From StreamMaster table
+    }
+}
